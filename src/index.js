306cebe4a4ecf63e55253dffa0653018a7ed6bd1
David Vera <david.esmeralda@gmail.com>
2022-09-28 08:50:25 -0500

    add amplify and uth

--- src/index.js
+++ src/index.js
@@ -5,11 +5,13 @@ import App from './App';
 import * as serviceWorkerRegistration from './serviceWorkerRegistration';
 import reportWebVitals from './reportWebVitals';
 
+import { BrowserRouter } from 'react-router-dom';
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
+  <BrowserRouter>
     <App />
-  </React.StrictMode>
+  </BrowserRouter >
 );
 
 // If you want your app to work offline and load faster, you can change
