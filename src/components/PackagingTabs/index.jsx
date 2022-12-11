8d623719881a24941dde9763719f2c2ef81406fb
Abdul Moiz Lakhani <lakhaniabdulmoiz@gmail.com>
2022-12-11 19:04:50 +0500

    - updated tab control

--- src/components/PackagingTabs/index.jsx
+++ src/components/PackagingTabs/index.jsx
@@ -10,6 +10,29 @@ import UnusedRawMaterial from "./UnusedRawMaterial";
 import WastedRawMaterial from "./WastedRawMaterial";
 import FinishProduction from "./FinishProduction";
 
+const tabsList = (activeTab) => [
+  {
+    label: <InventoryIcon fontSize="large" />,
+    variant: "contained",
+    disabled: activeTab < 0,
+  },
+  {
+    label: <AssignmentLateIcon fontSize="large" />,
+    variant: "contained",
+    disabled: activeTab < 1,
+  },
+  {
+    label: <DeleteForeverIcon fontSize="large" />,
+    variant: "contained",
+    disabled: activeTab < 2,
+  },
+  {
+    label: <AssignmentTurnedInIcon fontSize="large" />,
+    variant: "contained",
+    disabled: activeTab < 3,
+  },
+];
+
 export default function PackagingTabs({
   productionDetail,
   activeTab,
@@ -22,10 +45,14 @@ export default function PackagingTabs({
         value={activeTab}
         onChange={(_, value) => setActiveTab(value)}
       >
-        <Tab label={<InventoryIcon fontSize="large" />} variant="contained" />
-        <Tab label={<AssignmentLateIcon fontSize="large" />} />
-        <Tab label={<DeleteForeverIcon fontSize="large" />} />
-        <Tab label={<AssignmentTurnedInIcon fontSize="large" />} />
+        {tabsList(activeTab).map((tab, i) => (
+          <Tab
+            key={`packaging-tab-${i}`}
+            label={tab.label}
+            variant={tab.variant}
+            disabled={tab.disabled}
+          />
+        ))}
       </Tabs>
       <TabPanel
         value={activeTab}
