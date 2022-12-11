72623c341eb4cb9be90aa65cf0248cbe80a3d621
Abdul Moiz Lakhani <lakhaniabdulmoiz@gmail.com>
2022-12-11 17:06:26 +0500

    - implemented api for unused raw materials quantity - implemented api for wasted raw materials quantity

--- src/components/PackagingTabs/UnusedRawMaterial.jsx
+++ src/components/PackagingTabs/UnusedRawMaterial.jsx
@@ -1,6 +1,7 @@
-import { useContext } from "react";
+import { useContext, useState, useEffect } from "react";
 import Typography from "@mui/material/Typography";
 import Accordion from "@mui/material/Accordion";
+import Button from "@mui/material/Button";
 import AccordionDetails from "@mui/material/AccordionDetails";
 import AccordionSummary from "@mui/material/AccordionSummary";
 import Box from "@mui/material/Box";
@@ -12,6 +13,8 @@ import FormControl from "@mui/material/FormControl";
 import InputLabel from "@mui/material/InputLabel";
 import { styled } from "@mui/material/styles";
 import ExpandMoreIcon from "@mui/icons-material/ExpandMore";
+import { DataStore } from "@aws-amplify/datastore";
+import { RawMaterialLot } from "../../models";
 import { FormsContext } from "../../contexts/FormsContext";
 import Images from "../../constants/Images";
 
@@ -29,6 +32,30 @@ const Item = styled(Paper)(({ theme }) => ({
 }));
 
 const DetailItem = ({ data }) => {
+  const [newQty, updateNewQty] = useState(0);
+  const [isLoading, setIsLoading] = useState(false);
+
+  const handleUpdateQty = async () => {
+    setIsLoading(true);
+    try {
+      const original = await DataStore.query(RawMaterialLot, data.id);
+      await DataStore.save(
+        RawMaterialLot.copyOf(original, (updated) => {
+          updated.notUsedQuantity = newQty;
+          updated.useQuantity = data.quantity - data.wasteQuantity - newQty;
+        })
+      );
+      setIsLoading(false);
+    } catch (error) {
+      console.log("ERROR UNUSED MATERIAL: ", error);
+      setIsLoading(false);
+    }
+  };
+
+  useEffect(() => {
+    updateNewQty(parseFloat(data.notUsedQuantity));
+  }, [data]);
+
   return (
     <Item>
       <Avatar
@@ -36,25 +63,39 @@ const DetailItem = ({ data }) => {
         src={Images.ingredient}
         sx={{ width: 56, height: 56 }}
       />
-      <Box marginLeft={2} display="flex" flexDirection="column">
+      <Box marginLeft={2} width="100%" display="flex" flexDirection="column">
         <Typography component="h6" color="black" fontWeight="bold">
           Lot Name: <span style={{ color: "#1976D2" }}> {data.name}</span>
         </Typography>
         <Typography component="h6" color="black" fontWeight="bold">
-          Lot Code: <span style={{ color: "#1976D2" }}> {data.code}</span>
+          Lot Code: <span style={{ color: "#1976D2" }}> {data.lotCode}</span>
         </Typography>
-        {/* <Typography component="h6" color="black" fontWeight="bold">
-          Unused Lot Quantity:
-          <span style={{ color: "#1976D2" }}> {data.quantity}</span>
-        </Typography> */}
-        <FormControl variant="filled" sx={{ marginTop: "1rem" }}>
-          <InputLabel htmlFor="pallet-name">Unused Quantity</InputLabel>
-          <FilledInput
-            value=""
-            onChange={() => {}}
-            endAdornment={<InputAdornment position="end">kg</InputAdornment>}
-          />
-        </FormControl>
+        <Box
+          display="flex"
+          alignItems="center"
+          justifyContent="space-between"
+          marginTop="1rem"
+        >
+          <FormControl variant="filled">
+            <InputLabel htmlFor="pallet-name">Unused Quantity</InputLabel>
+            <FilledInput
+              value={newQty}
+              onChange={(ev) => {
+                const { value } = ev.target;
+                updateNewQty(parseFloat(value || 0));
+              }}
+              endAdornment={<InputAdornment position="end">kg</InputAdornment>}
+            />
+          </FormControl>
+          <Button
+            size="small"
+            variant="contained"
+            disabled={data.notUsedQuantity === newQty || isLoading}
+            onClick={handleUpdateQty}
+          >
+            {isLoading ? "Saving..." : "Save"}
+          </Button>
+        </Box>
       </Box>
     </Item>
   );
